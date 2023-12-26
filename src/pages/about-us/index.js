482c6f4bbb0108dcf23d9e72a1a6eb5d6ee25a07
sandhyaginare <Sandhya.ginare@vkaps.com>
2023-12-26 16:59:59 +0530

    about us page

---
+++ src/pages/about-us/index.js
@@ -0,0 +1,56 @@
+import Footer from "@/components/Footer";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <div className="text-center mt-8 px-[8rem]">
+        <h2 className="text-bold text-3xl my-5">About Us</h2>
+        <div>
+          <p className="my-5">
+            Welcome to Serenity, a sanctuary for wellness and self-discovery
+            through the ancient art of yoga.
+          </p>
+          <p className="my-5">
+            At Serenity, we believe that yoga is more than just a physical
+            practice; it's a journey of self-exploration, mindfulness, and
+            holistic well-being. Founded by passionate yogis John Doe, our
+            studio is a manifestation of our commitment to creating a space
+            where individuals can reconnect with their bodies, minds, and
+            spirits.
+          </p>
+          <p className="my-5">
+            Our classes are designed to cater to practitioners of all levels,
+            from beginners to experienced yogis, offering a diverse range of
+            styles including Hatha, Vinyasa, Yin, and more. Led by our team of
+            experienced and certified instructors, each session is crafted to
+            nurture a harmonious balance between strength, flexibility, and
+            inner peace.
+          </p>
+          <p className="my-5">
+            Beyond the physical practice, Serenity is a community hub where
+            like-minded individuals come together to support each other on their
+            wellness journeys. We foster a warm, inclusive, and welcoming
+            environment where everyone feels embraced and empowered.
+          </p>
+          <p className="my-5">
+            Our mission extends beyond the studio walls. We are committed to
+            spreading the transformative benefits of yoga beyond our immediate
+            community through workshops, retreats, and outreach programs aimed
+            at promoting wellness and mindfulness in diverse settings.
+          </p>
+          <p className="my-5">
+            At Serenity, we aspire to be more than just a yoga studio; we're a
+            catalyst for personal growth, healing, and empowerment. Join us on
+            this transformative journey as we explore the path to a balanced and
+            vibrant life, both on and off the mat.
+          </p>
+          <p className="my-5">Thank you for being a part of our community.</p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default index;
